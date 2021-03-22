47a7d73b780c156be6d55ce612b0ec71cb23f639
ljzyal <ljzyal@gmail.com>
2021-03-22 09:54:59 -0700

    Updates

---
+++ precache-manifest.1091851c48bcdbb5fa24752f94c9640c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "478579a40b99f096a637",
+    "url": "/NET/static/js/runtime~main.148020b3.js"
+  },
+  {
+    "revision": "7b5a21b627325fa8e8d8",
+    "url": "/NET/static/js/main.909d6de8.chunk.js"
+  },
+  {
+    "revision": "48c0e035da613b7370d5",
+    "url": "/NET/static/js/2.9ec3f89c.chunk.js"
+  },
+  {
+    "revision": "7b5a21b627325fa8e8d8",
+    "url": "/NET/static/css/main.bbe9f02d.chunk.css"
+  },
+  {
+    "revision": "48c0e035da613b7370d5",
+    "url": "/NET/static/css/2.1e9c9b31.chunk.css"
+  },
+  {
+    "revision": "adc6750ef496d04c5af17d4e9e8d29f2",
+    "url": "/NET/index.html"
+  }
+];
